b97f8aee879728c9713fc2421b3d60331cc37c5a
11102Jamd <jalejandromd17@gmail.com>
2025-09-04 20:57:25 -0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet} from 'react-router-dom';
 import { AuthProvider } from './components/context/AuthContext';
